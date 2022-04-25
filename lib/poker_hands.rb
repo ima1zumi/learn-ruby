91af2819b3e64ea0678894fe8e8159af3f5e7fb5
ima1zumi <mariimaizumi5@gmail.com>
2022-04-25 23:58:27 +0900

    Add spec

--- lib/poker_hands.rb
+++ lib/poker_hands.rb
@@ -1,3 +1,5 @@
+Card = Struct.new(:suit, :number)
+
 class PokerHands
   def self.judge(cards)
   end
--- spec/lib/poker_hands_spec.rb
+++ spec/lib/poker_hands_spec.rb
@@ -1,19 +1,56 @@
 require 'rspec'
 require_relative '../../lib/poker_hands.rb'
 
+# ref: https://en.wikipedia.org/wiki/Texas_hold_%27em
+
 RSpec.describe PokerHands do
-  context 'Straight flush' do
+  context 'Royal Flush' do
+    let(:cards) do
+      [
+        Card.new(:spade, 'A'),
+        Card.new(:spade, 'K'),
+        Card.new(:spade, 'Q'),
+        Card.new(:spade, 'J'),
+        Card.new(:spade, '10')
+      ]
+    end
+
     it do
-      expected = 'Straight flush'
-      cards = %w(Spade5 Spade6 Spade7 Spade8 Spade9)
+      expected = 'Royal Flush'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
+
+  context 'Straight Flush' do
+    let(:cards) do
+      [
+        Card.new(:spade, '2'),
+        Card.new(:spade, '3'),
+        Card.new(:spade, '4'),
+        Card.new(:spade, '5'),
+        Card.new(:spade, '6')
+      ]
+    end
+
+    it do
+      expected = 'Straight Flush'
       expect(PokerHands.judge(cards)).to eq(expected)
     end
   end
 
   context 'Four of a kind' do
+    let(:cards) do
+      [
+        Card.new(:spade, '6'),
+        Card.new(:diamond, '6'),
+        Card.new(:heart, '6'),
+        Card.new(:club, '1'),
+        Card.new(:spade, 'K')
+      ]
+    end
+
     it do
       expected = 'Four of a kind'
-      cards = %w(Spade5 Spade6 Spade7 Spade8 Spade9)
       expect(PokerHands.judge(cards)).to eq(expected)
     end
   end
