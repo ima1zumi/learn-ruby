3b6aae8dd4193eb3650eba048cae378a6fc02ad8
ima1zumi <mariimaizumi5@gmail.com>
2022-04-26 23:17:00 +0900

    Add spec

--- spec/lib/poker_hands_spec.rb
+++ spec/lib/poker_hands_spec.rb
@@ -54,4 +54,123 @@ RSpec.describe PokerHands do
       expect(PokerHands.judge(cards)).to eq(expected)
     end
   end
+
+  context 'Full house' do
+    let(:cards) do
+      [
+        Card.new(:club, 'K'),
+        Card.new(:heart, 'K'),
+        Card.new(:diamond, 'K'),
+        Card.new(:club, '7'),
+        Card.new(:spade, '7')
+      ]
+    end
+
+    it do
+      expected = 'Full house'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
+
+  context 'Flush' do
+    let(:cards) do
+      [
+        Card.new(:club, 'K'),
+        Card.new(:club, 'Q'),
+        Card.new(:club, '9'),
+        Card.new(:club, '8'),
+        Card.new(:club, '2')
+      ]
+    end
+
+    it do
+      expected = 'Flush'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
+
+  context 'Straight' do
+    let(:cards) do
+      [
+        Card.new(:club, '3'),
+        Card.new(:heart, '4'),
+        Card.new(:diamond, '5'),
+        Card.new(:club, '6'),
+        Card.new(:spade, '7')
+      ]
+    end
+
+    it do
+      expected = 'Straight'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
+
+  context 'Three of a kind' do
+    let(:cards) do
+      [
+        Card.new(:club, 'K'),
+        Card.new(:heart, 'K'),
+        Card.new(:diamond, 'K'),
+        Card.new(:club, '7'),
+        Card.new(:spade, '5')
+      ]
+    end
+
+    it do
+      expected = 'Three of a kind'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
+
+  context 'Two pairs' do
+    let(:cards) do
+      [
+        Card.new(:club, 'K'),
+        Card.new(:heart, 'K'),
+        Card.new(:diamond, '7'),
+        Card.new(:club, '7'),
+        Card.new(:spade, '5')
+      ]
+    end
+
+    it do
+      expected = 'Two pairs'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
+
+  context 'Pair' do
+    let(:cards) do
+      [
+        Card.new(:club, 'K'),
+        Card.new(:heart, 'K'),
+        Card.new(:diamond, '7'),
+        Card.new(:club, '2'),
+        Card.new(:spade, '5')
+      ]
+    end
+
+    it do
+      expected = 'Pair'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
+
+  context 'Highcard' do
+    let(:cards) do
+      [
+        Card.new(:club, '10'),
+        Card.new(:heart, '4'),
+        Card.new(:diamond, '7'),
+        Card.new(:club, 'K'),
+        Card.new(:spade, '2')
+      ]
+    end
+
+    it do
+      expected = 'Highcard'
+      expect(PokerHands.judge(cards)).to eq(expected)
+    end
+  end
 end
